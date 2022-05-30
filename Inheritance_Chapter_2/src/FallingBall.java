572a395f54fabcb82aee3b4f6aad8f1a40a7bb5a
Lucas Campos <achcarlucas@hotmail.com>
2022-05-30 20:00:41 -0300

    Class Particle e FallingBall

---
+++ Inheritance_Chapter_2/src/FallingBall.java
@@ -0,0 +1,28 @@
+package src;
+
+public class FallingBall extends Particle {
+    final static double g = 9.8f;
+    // initial position and velocity
+    private double y0 = 0;
+    private double v0 = 0;
+
+    public FallingBall(double y0, double v0) {
+        System.out.println("A new Falling Particle object is created");
+        this.y = this.y0 = y0;
+        this.v = this.v0 = v0;
+    }
+
+    public void step() {
+        y = y + v * t;
+        v = v - g * t;
+        t = t + dt;
+    }
+
+    public double analyticPosition() {
+        return y0 + v0 * t - (g * t * t) / 2.0;
+    }
+
+    public double analyticVelocity() {
+        return v0 + v * t;
+    }
+}
---
+++ Inheritance_Chapter_2/src/Particle.java
@@ -0,0 +1,14 @@
+package src;
+
+abstract public class Particle {
+    double y, v, t;
+    double dt;
+
+    public Particle() {
+        System.out.println("A new Particle is created");
+    }
+
+    abstract protected void step();
+    abstract protected double analyticPosition();
+    abstract protected double analyticVelocity();
+}
